0246b694169803c3ebe18e2a74b776a9496e9fd8
marcelovinicius2 <marcelovinicius2@hotmail.com>
2022-06-21 00:21:56 -0300

    onne commit

---
+++ teste.js
@@ -0,0 +1,15 @@
+console.log("hello word!!!");
+
+function returnEvenValues(array) {
+    let evenNums = [];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+
+            evenNums.push(array[i]);
+        } else {
+            console.log(`${array[i]}, não é par);
+    }
+    console.log(evenNums);
+       
+    }
+}
