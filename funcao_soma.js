c02218e4bd224643cc9ffa45874c609348872bf6
Marcelo Vinícius <32198245+marcelovinicius2@users.noreply.github.com>
2022-11-28 19:56:05 -0300

    criacao1

---
+++ funcao_soma.js
@@ -0,0 +1,7 @@
+//treinando Funções no curso Digital Innovaton One - DIO, 28/11/22
+
+function soma(a, b) { /*função criada/declarada*/
+    console.log(a + b);
+    return (a + b)
+}
+soma(5, 7); /*função chamada*/
